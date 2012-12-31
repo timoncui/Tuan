444636668682d13378cc247f6c296314ba46fac8
Lixing Huang <huanglx1984@hotmail.com>
2012-12-30 18:49:03 -0800

    make main.py run repeatedly

--- main.py
+++ main.py
@@ -8,27 +8,31 @@ import miner
 import find_deal_num
 import datetime
 
-start_time = datetime.datetime.now()
+while True:
+	start_time = datetime.datetime.now()
 
-sources = ["dida", "dianping", "ftuan", "manzuo", "meituan", "nuomi", "wowo", "wuba"]
+	sources = ["dida", "dianping", "ftuan", "manzuo", "meituan", "nuomi", "wowo", "wuba"]
 
-for source in sources:
-	print "start crawling deals from", source
-	async_pipeline.main(source, None, None)
-	find_deal_num.main(source, True)
-	miner.main(source, True)
+	for source in sources:
+		print "start crawling deals from", source
+		async_pipeline.main(source, None, None)
+		find_deal_num.main(source, True)
+		miner.main(source, True)
 
-print "start crawling deals from lashou"
-pipeline.main("lashou", None, None)
-find_deal_num.main("lashou", True)
-miner.main("lashou", True)
+	print "start crawling deals from lashou"
+	pipeline.main("lashou", None, None)
+	find_deal_num.main("lashou", True)
+	miner.main("lashou", True)
 
-end_time = datetime.datetime.now()
-duration = end_time - start_time
-print "====================="
-print "The task takes", duration.seconds, "seconds to finish"
-print "====================="
+	end_time = datetime.datetime.now()
+	duration = end_time - start_time
+	print "====================="
+	print "The task takes", duration.seconds, "seconds to finish"
+	print "====================="
 
-fhandler = open("finish_log.txt", "a")
-fhandler.write(str(duration.seconds) + "\n")
-fhandler.close()
+	fhandler = open("finish_log.txt", "a")
+	fhandler.write(str(duration.seconds) + "\n")
+	fhandler.close()
+
+	if os.path.exists("main_term_sig"):
+		break
